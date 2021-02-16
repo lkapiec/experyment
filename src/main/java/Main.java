b3576d01c39fd1b2b843f44f8f11aad99f8a0018
Łukasz Kapiec <lkapiec@poczta.ecolomatic.eu>
2021-02-16 11:15:39 +0100

    Initial project update, mostly for CI/GIT/Jenkins automation and deployment

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Primary primary = new Primary();
+
+        System.out.println("Witaj swiecie!");
+        System.out.println("Suma wynosi = " + primary.sumValues(2,3));
+    }
+}
---
+++ src/main/java/Primary.java
@@ -0,0 +1,7 @@
+public class Primary {
+
+    public int sumValues(int a, int b)
+    {
+        return a + b;
+    }
+}
