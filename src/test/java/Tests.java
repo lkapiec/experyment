d35974ca42b172f5184149ae85c4798ed9af89ec
lkapiec <lkapiec@gmail.com>
2021-02-17 15:59:23 +0100

    make one test to be more usable ;) still continue on pom.xml

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,12 +1,13 @@
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Tests {
-
-
     @Test
     public void FirstTest()
     {
         System.out.println("test1 executed hurray!");
+        Primary primary = new Primary();
+        Assert.assertEquals(primary.sumValues(1,3),4);
     }
 
     @Test
