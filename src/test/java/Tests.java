b4a2c067aab045f95ee29b7fb8057f0ee520b8d7
lkapiec <lkapiec@gmail.com>
2021-02-17 13:27:02 +0100

    some experiments with pom.xml and jar files

---
+++ src/test/java/Tests.java
@@ -0,0 +1,17 @@
+import org.testng.annotations.Test;
+
+public class Tests {
+
+
+    @Test
+    public void FirstTest()
+    {
+        System.out.println("test1 executed hurray!");
+    }
+
+    @Test
+    public void SecondTest()
+    {
+        System.out.println("test2 executed hurray!");
+    }
+}
